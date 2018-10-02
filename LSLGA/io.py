2978fd2bb14beda53bbccd79e418be51bfe9d5d1
John Moustakas <john.moustakas@gmail.com>
2018-10-02 14:56:26 -0400

    more i/o cleanup

--- LSLGA/io.py
+++ LSLGA/io.py
@@ -76,71 +76,14 @@ def read_parent(columns=None, dr=None, kd=False, verbose=False):
     if kd:
         from astrometry.libkd.spherematch import tree_open
         parent = tree_open(parentfile, 'largegals')
-        
+        if verbose:
+            print('Read {} galaxies from KD catalog {}'.format(parent.n, parentfile))
     else:
         parent = Table(fitsio.read(parentfile, ext=extname, columns=columns, lower=True))
-        
-    if verbose:
-        print('Read {} objects from {} [{}]'.format(len(parent), parentfile))
+        if verbose:
+            print('Read {} galaxies from {}'.format(len(parent), parentfile))
 
     return parent
-
-def read_sample(first=None, last=None, dr='dr6-dr7', sfhgrid=1,
-                isedfit_lsphot=False, isedfit_sdssphot=False,
-                isedfit_lhphot=False, satellites=False,
-                kcorr=False, verbose=False):
-    """Read the sample.
-
-    """
-    if satellites:
-        prefix = 'satellites'
-    else:
-        prefix = 'centrals'
-
-    if isedfit_lsphot:
-        samplefile = os.path.join(sample_dir(), '{}-sfhgrid{:02d}-lsphot-{}.fits'.format(prefix, sfhgrid, dr))
-    elif isedfit_sdssphot:
-        samplefile = os.path.join(sample_dir(), '{}-sfhgrid{:02d}-sdssphot-dr14.fits'.format(prefix, sfhgrid))
-    elif isedfit_lhphot:
-        samplefile = os.path.join(sample_dir(), '{}-sfhgrid{:02d}-lhphot.fits'.format(prefix, sfhgrid))
-    else:
-        samplefile = os.path.join(sample_dir(), 'LSLGA-{}-{}.fits'.format(prefix, dr))
-        
-    if not os.path.isfile(samplefile):
-        print('File {} not found.'.format(samplefile))
-        return None
-
-    if first and last:
-        if first > last:
-            print('Index first cannot be greater than index last, {} > {}'.format(first, last))
-            raise ValueError()
-
-    info = fitsio.FITS(samplefile)
-    nrows = info[1].get_nrows()
-
-    if first is None:
-        first = 0
-    if last is None:
-        last = nrows
-    if first == last:
-        last = last + 1
-
-    rows = np.arange(first, last)
-
-    if kcorr:
-        ext = 2
-    else:
-        ext = 1
-
-    sample = Table(info[1].read(rows=rows, ext=ext))
-    if verbose:
-        if len(rows) == 1:
-            print('Read galaxy index {} from {}'.format(first, samplefile))
-        else:
-            print('Read galaxy indices {} through {} (N={}) from {}'.format(
-                first, last-1, len(sample), samplefile))
-            
-    return sample
 
 def read_tycho(magcut=12, verbose=True):
     """Read the Tycho 2 catalog.
@@ -155,7 +98,7 @@ def read_tycho(magcut=12, verbose=True):
     # Radius of influence; see eq. 9 of https://arxiv.org/pdf/1203.6594.pdf
     tycho['radius'] = (0.0802*(tycho['mag_bt'])**2 - 1.860*tycho['mag_bt'] + 11.625) / 60 # [degree]
     
-    return tycho    
+    return tycho
 
 def read_hyperleda(verbose=True):
     """Read the Hyperleda catalog.
