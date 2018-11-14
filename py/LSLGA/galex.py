fixed bug in image units

py/LSLGA/galex.py
@@ -77,7 +77,7 @@ def _galex_rgb_moustakas(imgs, **kwargs):
     red   *= 0.085
     green *= 0.095
     blue  *= 0.08
-    nonlinearity = 0.5 #1.0 # 2.5
+    nonlinearity = 0.5 # 1.0 # 2.5
     radius = red + green + blue
     val = np.arcsinh(radius * nonlinearity) / nonlinearity
     with np.errstate(divide='ignore', invalid='ignore'):
@@ -171,10 +171,6 @@ def galex_coadds(onegal, galaxy=None, radius=30, pixscale=1.5,
     if output_dir is None:
         output_dir = '.'
 
-    _galex_rgb = _galex_rgb_moustakas
-    #_galex_rgb = _galex_rgb_dstn
-    #_galex_rgb = _galex_rgb_official
-
     W = H = np.ceil(2 * radius / pixscale).astype('int') # [pixels]
     targetwcs = Tan(onegal['RA'], onegal['DEC'], (W+1) / 2.0, (H+1) / 2.0,
                     -pixscale / 3600.0, 0.0, 0.0, pixscale / 3600.0, float(W), float(H))
@@ -223,7 +219,6 @@ def galex_coadds(onegal, galaxy=None, radius=30, pixscale=1.5,
 
         for src in srcs:
             src.setBrightness(NanoMaggies(**{band: 1}))
-        #srcs_nocentral = np.array(srcs)[nocentral].tolist()
 
         for j in J:
             brick = galex_tiles[j]
@@ -258,7 +253,7 @@ def galex_coadds(onegal, galaxy=None, radius=30, pixscale=1.5,
 
             tie = np.ones_like(timg)  ## HACK!
             #hdr = fitsio.read_header(fn)
-            #zp = hdr['
+            #zp = hdr['']
             zp = zps[band]
             photocal = LinearPhotoCal( NanoMaggies.zeropointToScale(zp), band=band)
             tsky = ConstantSky(0.0)
@@ -274,6 +269,7 @@ def galex_coadds(onegal, galaxy=None, radius=30, pixscale=1.5,
             tim = Image(data=timg, inverr=tie, psf=tpsf, wcs=twcs, sky=tsky,
                         photocal=photocal, name='GALEX ' + band + brick.brickname)
 
+            ## Build the model image with and without the central galaxy model.
             tractor = Tractor([tim], srcs)
             mod = tractor.getModelImage(0)
             tractor.freezeParam('images')
@@ -283,19 +279,6 @@ def galex_coadds(onegal, galaxy=None, radius=30, pixscale=1.5,
             srcs_nocentral = np.array(srcs)[nocentral].tolist()
             tractor_nocentral = Tractor([tim], srcs_nocentral)
             mod_nocentral = tractor_nocentral.getModelImage(0)
-
-            ## Build the model image with and without the central galaxy model.
-            #def _galex_mod(tim, use_srcs):
-            #    tractor = Tractor([tim], use_srcs)
-            #    mod = tractor.getModelImage(0)
-            #    tractor.freezeParam('images')
-            #    #print('Params:')
-            #    #tractor.printThawedParams()
-            #    tractor.optimize_forced_photometry(priors=False, shared_params=False)
-            #    mod = tractor.getModelImage(0)
-            #    return mod
-            #mod = _galex_mod(tim, srcs)
-            #mod_nocentral = _galex_mod(tim, srcs_nocentral)
 
             comod[Yo, Xo] += wt * mod[Yi-y0, Xi-x0]
             comod_nocentral[Yo, Xo] += wt * mod_nocentral[Yi-y0, Xi-x0]
@@ -318,13 +301,22 @@ def galex_coadds(onegal, galaxy=None, radius=30, pixscale=1.5,
         comods_nocentral.append(comod_nocentral)
         coimgs_central.append(coimg_central)
 
-        # Write out the final images with and without the central.
+        # Write out the final images with and without the central, making sure
+        # to apply the zeropoint to go from counts/s to AB nanomaggies.
+        # https://asd.gsfc.nasa.gov/archive/galex/FAQ/counts_background.html
         for thisimg, imtype in zip( (coimg, comod, comod_nocentral),
                                 ('image', 'model', 'model-nocentral') ):
             fitsfile = os.path.join(output_dir, '{}-{}-{}.fits'.format(galaxy, imtype, niceband))
             if verbose:
                 print('Writing {}'.format(fitsfile))
-            fitsio.write(fitsfile, thisimg, clobber=True)
+            fitsio.write(fitsfile, thisimg * 10**(-0.4 * (zp - 22.5)), clobber=True)
+
+    # Build a color mosaic (but note that the images here are in units of
+    # background-subtracted counts/s).
+
+    _galex_rgb = _galex_rgb_moustakas
+    #_galex_rgb = _galex_rgb_dstn
+    #_galex_rgb = _galex_rgb_official
 
     for imgs, imtype in zip( (coimgs, comods, coresids, comods_nocentral, coimgs_central),
                              ('image', 'model', 'resid', 'model-nocentral', 'image-central') ):

py/LSLGA/qa.py
@@ -317,7 +317,7 @@ def qa_curveofgrowth(ellipsefit, png=None, verbose=True):
         mag = 22.5-2.5*np.log10(flux[good])
         ax.plot(sma, mag, label=filt)
 
-        print(filt, np.mean(mag[-5:]))
+        #print(filt, np.mean(mag[-5:]))
         #print(filt, mag[-5:], np.mean(mag[-5:])
         #print(filt, np.min(mag))
 

py/LSLGA/unwise.py
@@ -11,6 +11,8 @@ import numpy as np
 def _unwise_to_rgb(imgs, bands=[1,2], mn=-1, mx=100, arcsinh=1.0):
     """Support routine to generate color unWISE images.
 
+    Note that the input images need to be in *Vega* nanomaggies!
+
     """
     img = imgs[0]
     H, W = img.shape
@@ -124,17 +126,19 @@ def unwise_coadds(onegal, galaxy=None, radius=30, pixscale=2.75,
 
     wbands = [1, 2, 3, 4]
     wanyband = 'w'
+    vega_to_ab = dict(w1=2.699, w2=3.339, w3=5.174, w4=6.620)
 
+    # Convert the AB WISE fluxes in the Tractor catalog to Vega nanomaggies so
+    # they're consistent with the coadds, below.
     for band in wbands:
         f = T.get('flux_w{}'.format(band))
-        f *= 10.**(0.4 * primhdr['WISEAB{}'.format(band)])
+        f *= 10**(0.4 * vega_to_ab['w{}'.format(band)])
 
     coimgs = [np.zeros((H, W), np.float32) for b in wbands]
     comods = [np.zeros((H, W), np.float32) for b in wbands]
     comods_nocentral = [np.zeros((H, W), np.float32) for b in wbands]
     con    = [np.zeros((H, W), np.uint8) for b in wbands]
 
-    #unwise_tims = {'w1': [], 'w2': [], 'w3': [], 'w4': []}
     for iband, band in enumerate(wbands):
 
         for ii, src in enumerate(srcs):
@@ -178,10 +182,18 @@ def unwise_coadds(onegal, galaxy=None, radius=30, pixscale=2.75,
             if len(Yo) == 0:
                 continue
 
-            coimgs[iband][Yo, Xo] += tim.getImage()[Yi, Xi]
+            # The models are already in AB nanomaggies, but the tiles / tims are
+            # in Vega nanomaggies, so convert them here.
+            coimgs[iband][Yo, Xo] += tim.getImage()[Yi, Xi] 
             comods[iband][Yo, Xo] += mod[Yi, Xi]
             comods_nocentral[iband][Yo, Xo] += mod_nocentral[Yi, Xi]
             con   [iband][Yo, Xo] += 1
+
+        ## Convert back to nanomaggies.
+        #vega2ab = vega_to_ab['w{}'.format(band)]
+        #coimgs[iband] *= 10**(-0.4 * vega2ab)
+        #comods[iband] *= 10**(-0.4 * vega2ab)
+        #comods_nocentral[iband] *= 10**(-0.4 * vega2ab)
 
     for img, mod, mod_nocentral, n in zip(coimgs, comods, comods_nocentral, con):
         img /= np.maximum(n, 1)
@@ -195,18 +207,20 @@ def unwise_coadds(onegal, galaxy=None, radius=30, pixscale=2.75,
     # (coimg_central).
     coimgs_central = [img-mod for img, mod in list(zip(coimgs, comods_nocentral))]
 
-    # Write out the final images with and without the central.
+    # Write out the final images with and without the central and converted into
+    # AB nanomaggies.
     for coadd, imtype in zip( (coimgs, comods, comods_nocentral),
                               ('image', 'model', 'model-nocentral') ):
         for img, band in zip(coadd, wbands):
+            vega2ab = vega_to_ab['w{}'.format(band)]
             fitsfile = os.path.join(output_dir, '{}-{}-W{}.fits'.format(galaxy, imtype, band))
             if verbose:
                 print('Writing {}'.format(fitsfile))
-            fitsio.write(fitsfile, img, clobber=True)
+            fitsio.write(fitsfile, img * 10**(-0.4 * vega2ab), clobber=True)
 
-    # Color WISE images --
+    # Generate color WISE images.
     kwa = dict(mn=-1, mx=100, arcsinh=0.5)
-    #kwa = dict(mn=-0.1, mx=2., arcsinh=1)
+    #kwa = dict(mn=-0.05, mx=1., arcsinh=0.5)
     #kwa = dict(mn=-0.1, mx=2., arcsinh=None)
 
     for imgs, imtype in zip( (coimgs, comods, coresids, comods_nocentral, coimgs_central),