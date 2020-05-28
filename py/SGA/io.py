fdd1689d5fdd726f1d3fafa7ae0145b846131a5b
John Moustakas <john.moustakas@gmail.com>
2020-05-28 19:41:40 -0400

    io cleanup

--- py/SGA/io.py
+++ py/SGA/io.py
@@ -1,8 +1,8 @@
 """
-LSLGA.io
-========
+SGA.io
+======
 
-Code to read and write the various LSLGA files.
+Code to read and write the various SGA files.
 
 """
 import os, warnings
@@ -15,30 +15,30 @@ import fitsio
 from astropy.table import Table, Column, hstack
 from astropy.io import fits
 
-def LSLGA_dir():
-    if 'LSLGA_DIR' not in os.environ:
-        print('Required ${LSLGA_DIR environment variable not set.')
+def SGA_dir():
+    if 'SGA_DIR' not in os.environ:
+        print('Required ${SGA_DIR environment variable not set.')
         raise EnvironmentError
-    return os.path.abspath(os.getenv('LSLGA_DIR'))
+    return os.path.abspath(os.getenv('SGA_DIR'))
 
 def analysis_dir():
-    adir = os.path.join(LSLGA_dir(), 'analysis')
+    adir = os.path.join(SGA_dir(), 'analysis')
     if not os.path.isdir(adir):
         os.makedirs(adir, exist_ok=True)
     return adir
 
 def sample_dir(version=None):
-    sdir = os.path.join(LSLGA_dir(), 'sample')
+    sdir = os.path.join(SGA_dir(), 'sample')
     if not os.path.isdir(sdir):
         os.makedirs(sdir, exist_ok=True)
     if version:
-        sdir = os.path.join(LSLGA_dir(), 'sample', version)
+        sdir = os.path.join(SGA_dir(), 'sample', version)
         if not os.path.isdir(sdir):
             os.makedirs(sdir, exist_ok=True)
     return sdir
 
 def paper1_dir(figures=True):
-    pdir = os.path.join(LSLGA_dir(), 'science', 'paper1')
+    pdir = os.path.join(SGA_dir(), 'science', 'paper1')
     if not os.path.ipdir(pdir):
         os.makedirs(pdir, exist_ok=True)
     if figures:
@@ -49,254 +49,14 @@ def paper1_dir(figures=True):
 
 def html_dir():
     #if 'NERSC_HOST' in os.environ:
-    #    htmldir = '/global/project/projectdirs/cosmo/www/temp/ioannis/LSLGA'
+    #    htmldir = '/global/project/projectdirs/cosmo/www/temp/ioannis/SGA'
     #else:
-    #    htmldir = os.path.join(LSLGA_dir(), 'html')
+    #    htmldir = os.path.join(SGA_dir(), 'html')
 
-    htmldir = os.path.join(LSLGA_dir(), 'html')
+    htmldir = os.path.join(SGA_dir(), 'html')
     if not os.path.isdir(htmldir):
         os.makedirs(htmldir, exist_ok=True)
     return htmldir
-
-def missing_files_groups(args, sample, size, htmldir=None):
-    """Simple task-specific wrapper on missing_files.
-
-    """
-    if args.coadds:
-        if args.sdss:
-            suffix = 'sdss-coadds'
-        else:
-            suffix = 'coadds'
-    elif args.custom_coadds:
-        if args.sdss:
-            suffix = 'sdss-custom-coadds'
-        else:
-            suffix = 'custom-coadds'
-    elif args.ellipse:
-        if args.sdss:
-            suffix = 'sdss-ellipse'
-        else:
-            suffix = 'ellipse'
-    elif args.sersic:
-        suffix = 'sersic'
-    elif args.sky:
-        suffix = 'sky'
-    elif args.htmlplots:
-        suffix = 'html'
-    else:
-        suffix = ''        
-
-    if suffix != '':
-        groups = missing_files(sample, filetype=suffix, size=size, sdss=args.sdss,
-                               clobber=args.clobber, htmldir=htmldir)
-    else:
-        groups = []        
-
-    return suffix, groups
-
-def missing_files(sample, filetype='coadds', size=1, htmldir=None,
-                  sdss=False, clobber=False):
-    """Find missing data of a given filetype."""    
-
-    if filetype == 'coadds':
-        filesuffix = '-pipeline-resid-grz.jpg'
-    elif filetype == 'custom-coadds':
-        filesuffix = '-custom-resid-grz.jpg'
-    elif filetype == 'ellipse':
-        filesuffix = '-ellipsefit.p'
-    elif filetype == 'sersic':
-        filesuffix = '-sersic-single.p'
-    elif filetype == 'html':
-        filesuffix = '-ccdpos.png'
-        #filesuffix = '-sersic-exponential-nowavepower.png'
-    elif filetype == 'sdss-coadds':
-        filesuffix = '-sdss-image-gri.jpg'
-    elif filetype == 'sdss-custom-coadds':
-        filesuffix = '-sdss-resid-gri.jpg'
-    elif filetype == 'sdss-ellipse':
-        filesuffix = '-sdss-ellipsefit.p'
-    else:
-        print('Unrecognized file type!')
-        raise ValueError
-
-    if type(sample) is astropy.table.row.Row:
-        ngal = 1
-    else:
-        ngal = len(sample)
-    indices = np.arange(ngal)
-    todo = np.ones(ngal, dtype=bool)
-
-    if filetype == 'html':
-        galaxy, _, galaxydir = get_galaxy_galaxydir(sample, htmldir=htmldir, html=True)
-    else:
-        galaxy, galaxydir = get_galaxy_galaxydir(sample, htmldir=htmldir)
-
-    for ii, (gal, gdir) in enumerate( zip(np.atleast_1d(galaxy), np.atleast_1d(galaxydir)) ):
-        checkfile = os.path.join(gdir, '{}{}'.format(gal, filesuffix))
-        if os.path.exists(checkfile) and clobber is False:
-            todo[ii] = False
-
-    if np.sum(todo) == 0:
-        return list()
-    else:
-        indices = indices[todo]
-        
-    return np.array_split(indices, size)
-
-def read_all_ccds(dr='dr8'):
-    """Read the CCDs files, treating DECaLS and BASS+MzLS separately.
-
-    """
-    from astrometry.libkd.spherematch import tree_open
-    #survey = LegacySurveyData()
-
-    drdir = os.path.join(sample_dir(), dr)
-
-    kdccds_north = []
-    for camera in ('90prime', 'mosaic'):
-        ccdsfile = os.path.join(drdir, 'survey-ccds-{}-{}.kd.fits'.format(camera, dr))
-        ccds = tree_open(ccdsfile, 'ccds')
-        print('Read {} CCDs from {}'.format(ccds.n, ccdsfile))
-        kdccds_north.append((ccdsfile, ccds))
-
-    ccdsfile = os.path.join(drdir, 'survey-ccds-decam-{}.kd.fits'.format(dr))
-    ccds = tree_open(ccdsfile, 'ccds')
-    print('Read {} CCDs from {}'.format(ccds.n, ccdsfile))
-    kdccds_south = (ccdsfile, ccds)
-
-    return kdccds_north, kdccds_south
-
-def get_run(onegal, radius_mosaic, pixscale, kdccds_north, kdccds_south, log=None):
-    """Determine the "run", i.e., determine whether we should use the BASS+MzLS CCDs
-    or the DECaLS CCDs file when running the pipeline.
-
-    """
-    from astrometry.util.util import Tan
-    from astrometry.libkd.spherematch import tree_search_radec
-    from legacypipe.survey import ccds_touching_wcs
-    
-    ra, dec = onegal['RA'], onegal['DEC']
-    if dec < 25:
-        run = 'decam'
-    elif dec > 40:
-        run = '90prime-mosaic'
-    else:
-        width = LSLGA.coadds._mosaic_width(radius_mosaic, pixscale)
-        wcs = Tan(ra, dec, width/2+0.5, width/2+0.5,
-                  -pixscale/3600.0, 0.0, 0.0, pixscale/3600.0, 
-                  float(width), float(width))
-
-        # BASS+MzLS
-        TT = []
-        for fn, kd in kdccds_north:
-            I = tree_search_radec(kd, ra, dec, 1.0)
-            if len(I) == 0:
-                continue
-            TT.append(fits_table(fn, rows=I))
-        if len(TT) == 0:
-            inorth = []
-        else:
-            ccds = merge_tables(TT, columns='fillzero')
-            inorth = ccds_touching_wcs(wcs, ccds)
-        
-        # DECaLS
-        fn, kd = kdccds_south
-        I = tree_search_radec(kd, ra, dec, 1.0)
-        if len(I) > 0:
-            ccds = fits_table(fn, rows=I)
-            isouth = ccds_touching_wcs(wcs, ccds)
-        else:
-            isouth = []
-
-        if len(inorth) > len(isouth):
-            run = '90prime-mosaic'
-        else:
-            run = 'decam'
-        print('Cluster RA, Dec={:.6f}, {:.6f}: run={} ({} north CCDs, {} south CCDs).'.format(
-            ra, dec, run, len(inorth), len(isouth)), flush=True, file=log)
-
-    return run
-
-def check_and_read_ccds(galaxy, survey, debug=False, logfile=None):
-    """Read the CCDs file generated by the pipeline coadds step.
-
-    """
-    ccdsfile_south = os.path.join(survey.output_dir, '{}-ccds-decam.fits'.format(galaxy))
-    ccdsfile_north = os.path.join(survey.output_dir, '{}-ccds-90prime-mosaic.fits'.format(galaxy))
-    if os.path.isfile(ccdsfile_south):
-        ccdsfile = ccdsfile_south
-    elif os.path.isfile(ccdsfile_north):
-        ccdsfile = ccdsfile_north
-    else:
-        if debug:
-            print('CCDs file {} not found.'.format(ccdsfile_south), flush=True)
-            print('CCDs file {} not found.'.format(ccdsfile_north), flush=True)
-            print('ERROR: galaxy {}; please check the logfile.'.format(galaxy), flush=True)
-        else:
-            with open(logfile, 'w') as log:
-                print('CCDs file {} not found.'.format(ccdsfile_south), flush=True, file=log)
-                print('CCDs file {} not found.'.format(ccdsfile_north), flush=True, file=log)
-                print('ERROR: galaxy {}; please check the logfile.'.format(galaxy), flush=True, file=log)
-        return False
-    survey.ccds = survey.cleanup_ccds_table(fits_table(ccdsfile))
-
-    # Check that coadds in all three grz bandpasses were generated in the
-    # previous step.
-    if ('g' not in survey.ccds.filter) or ('r' not in survey.ccds.filter) or ('z' not in survey.ccds.filter):
-        if debug:
-            print('Missing grz coadds...skipping.', flush=True)
-            print('ERROR: galaxy {}; please check the logfile.'.format(galaxy), flush=True)
-        else:
-            with open(logfile, 'w') as log:
-                print('Missing grz coadds...skipping.', flush=True, file=log)
-                print('ERROR: galaxy {}; please check the logfile.'.format(galaxy), flush=True, file=log)
-        return False
-    return True
-
-def get_galaxy_galaxydir(cat, analysisdir=None, htmldir=None, html=False):
-    """Retrieve the galaxy name and the (nested) directory.
-
-    """
-    import astropy
-    import healpy as hp
-    from LSLGA.misc import radec2pix
-    
-    nside = 8 # keep hard-coded
-    
-    if analysisdir is None:
-        analysisdir = analysis_dir()
-    if htmldir is None:
-        htmldir = html_dir()
-
-    def get_healpix_subdir(nside, pixnum, analysisdir):
-        subdir = os.path.join(str(pixnum // 100), str(pixnum))
-        return os.path.abspath(os.path.join(analysisdir, str(nside), subdir))
-
-    if type(cat) is astropy.table.row.Row:
-        ngal = 1
-        galaxy = [cat['GALAXY']]
-        pixnum = [radec2pix(nside, cat['RA'], cat['DEC'])]
-    else:
-        ngal = len(cat)
-        galaxy = np.array([gg.decode('utf-8') for gg in cat['GALAXY'].data])
-        pixnum = radec2pix(nside, cat['RA'], cat['DEC']).data
-
-    galaxydir = np.array([os.path.join(get_healpix_subdir(nside, pix, analysisdir), gal)
-                          for pix, gal in zip(pixnum, galaxy)])
-    if html:
-        htmlgalaxydir = np.array([os.path.join(get_healpix_subdir(nside, pix, htmldir), gal)
-                                  for pix, gal in zip(pixnum, galaxy)])
-
-    if ngal == 1:
-        galaxy = galaxy[0]
-        galaxydir = galaxydir[0]
-        if html:
-            htmlgalaxydir = htmlgalaxydir[0]
-
-    if html:
-        return galaxy, galaxydir, htmlgalaxydir
-    else:
-        return galaxy, galaxydir
 
 def parent_version(version=None):
     """Version of the parent catalog.
@@ -325,13 +85,13 @@ def get_parentfile(version=None, kd=False):
     else:
         suffix = 'fits'
         
-    parentfile = os.path.join(sample_dir(version=version), 'LSLGA-{}.{}'.format(version, suffix))
+    parentfile = os.path.join(sample_dir(version=version), 'SGA-{}.{}'.format(version, suffix))
 
     return parentfile
 
 def read_parent(columns=None, verbose=False, first=None, last=None,
                 version=None, chaos=False):
-    """Read the LSLGA parent catalog.
+    """Read the SGA parent catalog.
 
     """
     if version is None:
@@ -526,164 +286,11 @@ def read_hyperleda(verbose=False, allwise=False, version=None):
 
     return leda
 
-def read_multiband(galaxy, galaxydir, band=('g', 'r', 'z'), refband='r',
-                   pixscale=0.262, galex_pixscale=1.5, unwise_pixscale=2.75,
-                   maskfactor=2.0):
-    """Read the multi-band images, construct the residual image, and then create a
-    masked array from the corresponding inverse variances image.  Finally,
-    convert to surface brightness by dividing by the pixel area.
-
-    """
-    from scipy.stats import sigmaclip
-    from scipy.ndimage.morphology import binary_dilation
-
-    # Dictionary mapping between filter and filename coded up in coadds.py,
-    # galex.py, and unwise.py (see the LSLGA product, too).
-    filt2imfile = {
-        'g':   ['custom-image', 'custom-model-nocentral', 'invvar'],
-        'r':   ['custom-image', 'custom-model-nocentral', 'invvar'],
-        'z':   ['custom-image', 'custom-model-nocentral', 'invvar'],
-        'FUV': ['image', 'model-nocentral'],
-        'NUV': ['image', 'model-nocentral'],
-        'W1':  ['image', 'model-nocentral'],
-        'W2':  ['image', 'model-nocentral'],
-        'W3':  ['image', 'model-nocentral'],
-        'W4':  ['image', 'model-nocentral']}
-        
-    filt2pixscale =  {
-        'g':   pixscale,
-        'r':   pixscale,
-        'z':   pixscale,
-        'FUV': galex_pixscale,
-        'NUV': galex_pixscale,
-        'W1':  unwise_pixscale,
-        'W2':  unwise_pixscale,
-        'W3':  unwise_pixscale,
-        'W4':  unwise_pixscale}
-
-    found_data = True
-    for filt in band:
-        for ii, imtype in enumerate(filt2imfile[filt]):
-            for suffix in ('.fz', ''):
-                imfile = os.path.join(galaxydir, '{}-{}-{}.fits{}'.format(galaxy, imtype, filt, suffix))
-                if os.path.isfile(imfile):
-                    filt2imfile[filt][ii] = imfile
-                    break
-            if not os.path.isfile(imfile):
-                print('File {} not found.'.format(imfile))
-                found_data = False
-
-    #tractorfile = os.path.join(galaxydir, '{}-tractor.fits'.format(galaxy))
-    #if os.path.isfile(tractorfile):
-    #    cat = Table(fitsio.read(tractorfile, upper=True))
-    #    print('Read {} sources from {}'.format(len(cat), tractorfile))
-    #else:
-    #    print('Missing Tractor catalog {}'.format(tractorfile))
-    #    found_data = False
-
-    data = dict()
-    if not found_data:
-        return data
-
-    for filt in band:
-        image = fitsio.read(filt2imfile[filt][0])
-        model = fitsio.read(filt2imfile[filt][1])
-
-        if len(filt2imfile[filt]) == 3:
-            invvar = fitsio.read(filt2imfile[filt][2])
-
-            # Mask pixels with ivar<=0. Also build an object mask from the model
-            # image, to handle systematic residuals.
-            mask = (invvar <= 0) # True-->bad, False-->good
-            
-            #if np.sum(mask) > 0:
-            #    invvar[mask] = 1e-3
-            #snr = model * np.sqrt(invvar)
-            #mask = np.logical_or( mask, (snr > 1) )
-
-            #sig1 = 1.0 / np.sqrt(np.median(invvar))
-            #mask = np.logical_or( mask, (image - model) > (3 * sig1) )
-
-        else:
-            mask = np.zeros_like(image).astype(bool)
-
-        # Can give a divide-by-zero error for, e.g., GALEX imaging
-        #with np.errstate(divide='ignore', invalid='ignore'):
-        with warnings.catch_warnings():
-            warnings.simplefilter('ignore')
-            with np.errstate(all='ignore'):
-                model_clipped, _, _ = sigmaclip(model, low=4.0, high=4.0)
-
-        #print(filt, 1-len(model_clipped)/image.size)
-        #if filt == 'W1':
-        #    pdb.set_trace()
-            
-        if len(model_clipped) > 0:
-            mask = np.logical_or( mask, model > 3 * np.std(model_clipped) )
-            #model_clipped = model
-        
-        mask = binary_dilation(mask, iterations=1) # True-->bad
-
-        thispixscale = filt2pixscale[filt]
-        data[filt] = (image - model) / thispixscale**2 # [nanomaggies/arcsec**2]
-        
-        #data['{}_mask'.format(filt)] = mask # True->bad
-        data['{}_masked'.format(filt)] = ma.masked_array(data[filt], mask)
-        ma.set_fill_value(data['{}_masked'.format(filt)], 0)
-
-    data['band'] = band
-    data['refband'] = refband
-    data['pixscale'] = pixscale
-
-    if 'NUV' in band:
-        data['galex_pixscale'] = galex_pixscale
-    if 'W1' in band:
-        data['unwise_pixscale'] = unwise_pixscale
-
-    return data
-
-def write_ellipsefit(galaxy, galaxydir, ellipsefit, verbose=False, noellipsefit=True):
-    """Pickle a dictionary of photutils.isophote.isophote.IsophoteList objects (see,
-    e.g., ellipse.fit_multiband).
-
-    """
-    if noellipsefit:
-        suffix = '-fixed'
-    else:
-        suffix = ''
-        
-    ellipsefitfile = os.path.join(galaxydir, '{}-ellipsefit{}.p'.format(galaxy, suffix))
-    if verbose:
-        print('Writing {}'.format(ellipsefitfile))
-    with open(ellipsefitfile, 'wb') as ell:
-        pickle.dump(ellipsefit, ell)
-
-def read_ellipsefit(galaxy, galaxydir, verbose=True, noellipsefit=True):
-    """Read the output of write_ellipsefit.
-
-    """
-    if noellipsefit:
-        suffix = '-fixed'
-    else:
-        suffix = ''
-
-    ellipsefitfile = os.path.join(galaxydir, '{}-ellipsefit{}.p'.format(galaxy, suffix))
-    try:
-        with open(ellipsefitfile, 'rb') as ell:
-            ellipsefit = pickle.load(ell)
-    except:
-        #raise IOError
-        if verbose:
-            print('File {} not found!'.format(ellipsefitfile))
-        ellipsefit = dict()
-
-    return ellipsefit
-
 def read_localgroup_dwarfs():
-    """Read the sample generated by bin/LSLGA-localgroup-dwarfs.
+    """Read the sample generated by bin/SGA-localgroup-dwarfs.
 
     """
-    dwarfsfile = os.path.join(sample_dir(), 'catalogs', 'LSLGA-dwarfs.fits')
+    dwarfsfile = os.path.join(sample_dir(), 'catalogs', 'SGA-dwarfs.fits')
     dwarfs = Table(fitsio.read(dwarfsfile, upper=True))
     print('Read {} Local Group dwarfs from {}'.format(len(dwarfs), dwarfsfile))
 
