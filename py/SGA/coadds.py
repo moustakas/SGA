682a126e4cddcfea1293370c3f629321b3e1d844
John Moustakas <john.moustakas@gmail.com>
2025-10-21 03:47:15 -0700

    add new --ngpu argument

--- py/SGA/coadds.py
+++ py/SGA/coadds.py
@@ -345,7 +345,7 @@ def custom_coadds(onegal, galaxy, survey, run, radius_mosaic_arcsec,
                   release=1000, pixscale=PIXSCALE, bands=GRIZ, mp=1, nsigma=None,
                   nsatur=2, racolumn='GROUP_RA', deccolumn='GROUP_DEC',
                   force_psf_detection=False, fit_on_coadds=False,
-                  use_gpu=False, threads_per_gpu=16, subsky_radii=None,
+                  use_gpu=False, ngpu=1, threads_per_gpu=8, subsky_radii=None,
                   just_coadds=False, missing_ok=False, force=False, cleanup=True,
                   unwise=True, galex=False, no_gaia=False, no_tycho=False,
                   verbose=False):
@@ -434,7 +434,7 @@ def custom_coadds(onegal, galaxy, survey, run, radius_mosaic_arcsec,
 
     # GPU stuff
     if use_gpu:
-        cmdargs += f'--use-gpu --threads-per-gpu={threads_per_gpu} --ngpu=1 --gpumode=2 '
+        cmdargs += f'--use-gpu --threads-per-gpu={threads_per_gpu} --ngpu={ngpu} --gpumode=2 '
 
     try:
         log.info(f'runbrick {cmdargs}')
