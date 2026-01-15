834ba795804744511b2f25bb66819b62cdb1f530
John Moustakas <john.moustakas@gmail.com>
2026-01-15 09:08:12 -0800

    revert to v0.50 to redo some of the ellipses

--- py/SGA/SGA.py
+++ py/SGA/SGA.py
@@ -106,10 +106,10 @@ def SGA_version(vicuts=False, nocuts=False, archive=False, parent=False):
         #version = 'v0.40'
 
         # tons of additional sample cleanup
-        #version = 'v0.50'
+        version = 'v0.50'
 
         # more cleanup
-        version = 'v0.60'
+        #version = 'v0.60'
     else:
         # parent-refcat, parent-ellipse, and final SGA2025
         #version = 'v0.10' # parent_version = v0.10
@@ -120,8 +120,8 @@ def SGA_version(vicuts=False, nocuts=False, archive=False, parent=False):
         #version = 'v0.22'  # parent_version = v0.21 --> v0.22
         #version = 'v0.30'  # parent_version = v0.22 --> v0.30
         #version = 'v0.40'  # parent_version = v0.30 --> v0.40
-        #version = 'v0.50'  # parent_version = v0.40 --> v0.50
-        version = 'v0.60'  # parent_version = v0.50 --> v0.60
+        version = 'v0.50'  # parent_version = v0.40 --> v0.50
+        #version = 'v0.60'  # parent_version = v0.50 --> v0.60
     return version
 
 
