88b7a74a7d3bd7982b3152fbbb051baea3edd241
John Moustakas <john.moustakas@gmail.com>
2026-04-06 07:12:27 -0700

    more VI

--- py/SGA/parent.py
+++ py/SGA/parent.py
@@ -4539,7 +4539,7 @@ def prepare_v130_ellipse(ell1, region, mindiam=0.5):
                   'WISEA J154301.82-275427.7', 'WISEA J191255.62-363231.3',
                   'WISEA J200648.91-533022.6', 'ESO 400- G 025', 'ESO 087- G 027',
                   'WISEA J120444.71+104642.2', 'UGCA 274', 'VCC 0909', 'ESO 443- G 020',
-                  'ESO 446- G 020', 'APMBGC 235+059-099', ]
+                  'ESO 446- G 020', 'APMBGC 235+059-099', 'IC 3639', ]
     if len(refit_list) > 0:
         refit_list = np.unique(refit_list)
         refit = np.isin(ell1['OBJNAME'], refit_list)
