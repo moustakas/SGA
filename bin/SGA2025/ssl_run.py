92fc130e35b5741126c4ae0e4c39345a904f198b
John Moustakas <john.moustakas@gmail.com>
2026-06-07 19:30:46 -0400

    refactor and cleanup of ssl scripts

--- bin/SGA2025/ssl_run.py
+++ bin/SGA2025/ssl_run.py
@@ -1,6 +1,30 @@
-from SSL_Match import ssl_match
+"""
+Run ssl_match inference on one or more HDF5 chunks.
 
-for i in range(92):
-    # Format the index number with leading zeros to read in each of the files
-    filename = f"/pscratch/sd/i/ioannis/SGA2025/ssl/v1/input2/ssl-parent-chunk{i:03d}-v1.hdf5"
-    ssl_match(filename)
+Usage:
+    python ssl_run.py chunk*.hdf5 --checkpoint resnet50.ckpt --output-dir ./results
+"""
+import argparse
+
+from SGA.ssl import ssl_match
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Run ssl_match on HDF5 chunk files.')
+    parser.add_argument('hdf5_files', nargs='+', help='HDF5 input file(s)')
+    parser.add_argument('--checkpoint', default='resnet50.ckpt',
+                        help='Path to the pre-trained resnet50.ckpt checkpoint')
+    parser.add_argument('--output-dir', default=None,
+                        help='Directory for results (default: same dir as each HDF5 file)')
+    parser.add_argument('--no-similarity', dest='similarity', action='store_false',
+                        help='Skip similarity-search output')
+    parser.add_argument('--threshold', type=float, default=0.5,
+                        help='Similarity threshold (default 0.5)')
+    args = parser.parse_args()
+
+    for path in args.hdf5_files:
+        ssl_match(path,
+                  checkpoint_path=args.checkpoint,
+                  output_dir=args.output_dir,
+                  similarity=args.similarity,
+                  threshold=args.threshold)
--- py/SGA/ssl_sort.py
+++ py/SGA/ssl_sort.py
@@ -1,123 +1,99 @@
-'''
-This script generates and saves the file that is needed to run the similarity search script.
-'''
+"""
+SGA.ssl_sort
+============
 
-import os
-import h5py
-import numpy as np
-import torch
-from ssl_legacysurvey.utils import load_data # Loading galaxy catalogue and image data from hdf5 file(s)
-from ssl_legacysurvey.utils import plotting_tools as plt_tools # Plotting images or catalogue info
-from ssl_legacysurvey.data_loaders import datamodules # Pytorch dataloaders and datamodules
-from ssl_legacysurvey.data_loaders import decals_augmentations # Augmentations for training
-from ssl_legacysurvey.data_analysis import dimensionality_reduction # PCA/UMAP functionality
-import matplotlib.pyplot as plt
-import numpy as np
-import torch
-import torchvision
-import pytorch_lightning as pl
-import h5py
-import argparse
-from pathlib import Path
+Generate 128-d UMAP representations for an HDF5 chunk to enable
+similarity search via the ssl-legacysurvey pipeline.
+
+Usage:
+    python ssl_sort.py chunk.hdf5 --checkpoint resnet50.ckpt --output-dir ./reps
+"""
 import os
 import sys
 import glob
-import math
-from pytorch_lightning import loggers as pl_loggers
-from pytorch_lightning.plugins import DDPPlugin
-from ssl_legacysurvey.moco.moco2_module import Moco_v2 
+import argparse
+import numpy as np
+import torch
+
+from ssl_legacysurvey.utils import load_data
 from ssl_legacysurvey.data_loaders import datamodules
-from ssl_legacysurvey.utils import format_logger
-from scripts import predict
-from ssl_legacysurvey.finetune import extract_model_outputs
-from scripts import similarity_search_nxn
-from sklearn.cluster import KMeans
-import psutil
-import os
-import time
-
-def ssl_match(path, threshold=0.5):
-    
-    # Load h5py file into dictionary
-    data_path = path
-    # with h5py.File(path) as F: 
-    #     nref = np.sum(F['ref'])
-
-    ##### extract out 
-    DDL = load_data.DecalsDataLoader(image_dir=data_path, npix_in=152)
-    gals = DDL.get_data(-1, fields=DDL.fields_available,npix_out=152) # -1 to load all galaxies
-
-    class Args: 
-        data_path = path
-        base = os.path.splitext(os.path.basename(data_path))[0]
-        gpu = True # Use GPU?
-        gpus = 1 # Number of gpus to use
-        num_nodes = 1
-        ngals_tot = gals['images'].shape[0]
-        # Read in the checkpoint file from when the SSl model was originally trained
-        checkpoint_path = 'resnet50.ckpt'
-        rep_dir = os.path.join(base, 'representations/compiled')
-        file_head = 'original'
-    
-    params = dict(vars(Args))
-    p = {}
-    for k, v in params.items():
-        p[k] = v
-    params = p
+from ssl_legacysurvey.data_analysis import dimensionality_reduction
+from ssl_legacysurvey.moco.moco2_module import Moco_v2
 
 
-    ####### Load model from checkpoint
-    model = Moco_v2.load_from_checkpoint(
-        checkpoint_path=params['checkpoint_path']
-        )
+def ssl_sort(path, checkpoint_path='resnet50.ckpt', output_dir=None, threshold=0.5):
+    """Compute 128-d UMAP representations for a single HDF5 chunk.
 
+    The representations are needed as input to the similarity search
+    script.  Results are saved as a .npy file alongside the HDF5.
+
+    Parameters
+    ----------
+    path : str
+        Path to an HDF5 file produced by build_ssl_legacysurvey().
+    checkpoint_path : str
+        Path to the pre-trained MoCo v2 ResNet-50 checkpoint.
+    output_dir : str, optional
+        Directory for the output .npy file.
+        Defaults to the directory containing `path`.
+    """
+    if output_dir is None:
+        output_dir = os.path.dirname(os.path.abspath(path))
+    os.makedirs(output_dir, exist_ok=True)
+
+    base = os.path.splitext(os.path.basename(path))[0]
+
+    DDL  = load_data.DecalsDataLoader(image_dir=path, npix_in=152)
+    gals = DDL.get_data(-1, fields=DDL.fields_available, npix_out=152)
+
+    params = {
+        'data_path':       path,
+        'base':            base,
+        'gpu':             torch.cuda.is_available(),
+        'gpus':            1,
+        'num_nodes':       1,
+        'ngals_tot':       gals['images'].shape[0],
+        'checkpoint_path': checkpoint_path,
+        'ssl_training':    False,
+        'jitter_lim':      0,
+        'augmentations':   'jcrg',
+        'file_head':       'original',
+    }
+
+    model    = Moco_v2.load_from_checkpoint(checkpoint_path=checkpoint_path)
     backbone = model.encoder_q
     backbone.fc = torch.nn.Identity()
-    
-    params['ssl_training'] = False
-    params['jitter_lim'] = 0 #determines the maximum value for the offset in the jitter augmentation
-    params['augmentations'] = 'jcrg' #adjust to whatever parameters you want, typically 'jcrg' or 'rrjc'
-    
-    transform = datamodules.DecalsTransforms(
-        params['augmentations'],
-        params
-    )
-    decals_dataloader = datamodules.DecalsDataset(
-        data_path,
-        None,
-        transform,
-        params,
-    )
-    # ngals can be subset of the dataset or the whole dataset
-    ngals =  gals['images'].shape[0]
-    im, label = decals_dataloader.__getitem__(0)
-    images = torch.empty((ngals, im.shape[0], im.shape[1], im.shape[2]), dtype=im.dtype)
+
+    transform = datamodules.DecalsTransforms(params['augmentations'], params)
+    dataset   = datamodules.DecalsDataset(path, None, transform, params)
+
+    ngals = gals['images'].shape[0]
+    im, _ = dataset[0]
+    images = torch.empty((ngals, *im.shape), dtype=im.dtype)
     for i in range(ngals):
-        images[i], _ = decals_dataloader.__getitem__(i)  
-    
+        images[i], _ = dataset[i]
+
     representations = backbone(images)
-
     if params['gpu']:
-        images, representations = images.detach(), representations.detach()
-    images, representations = images.numpy(), representations.numpy()
-    ###### smash down to 128 dimensions instead of 2048
+        representations = representations.detach()
+    representations = representations.numpy()
 
-    reps_128, reps_128_trans = dimensionality_reduction.umap_transform(representations, n_components=128, metric='cosine')
-    
-    np.save(f"/pscratch/sd/s/sgmoore1/ssl-legacysurvey/{params['rep_dir']}/{params['file_head']}_{0 :09d}_{params['ngals_tot']:09d}.npy", reps_128)
+    reps_128, _ = dimensionality_reduction.umap_transform(
+        representations, n_components=128, metric='cosine')
 
-
-
-
-
-
+    out_path = os.path.join(
+        output_dir,
+        f"{params['file_head']}_{0:09d}_{params['ngals_tot']:09d}.npy")
+    np.save(out_path, reps_128)
+    print(f'Wrote {out_path}')
 
 
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Compute 128-d UMAP representations for ssl similarity search.')
+    parser.add_argument('hdf5_files', nargs='+', help='HDF5 chunk file(s)')
+    parser.add_argument('--checkpoint', default='resnet50.ckpt', help='Path to resnet50.ckpt')
+    parser.add_argument('--output-dir', default=None, help='Output directory for .npy files')
+    args = parser.parse_args()
 
-
-
-
-
-
-
-        
+    for path in args.hdf5_files:
+        ssl_sort(path, checkpoint_path=args.checkpoint, output_dir=args.output_dir)
