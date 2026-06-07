f314e584e910356913c87d67862e5814e4b164f2
John Moustakas <john.moustakas@gmail.com>
2026-06-07 19:58:44 -0400

    default SGA_DATA and SGA_HTML paths at NERSC

--- etc/activate-sga.sh
+++ etc/activate-sga.sh
@@ -9,4 +9,8 @@ SGA_PREFIX=/global/common/software/desi/users/ioannis/SGA
 unset PYTHONPATH
 module purge
 
+export SGA_DIR=${SGA_DIR:-/dvs_ro/cfs/cdirs/cosmo/data/sga/2025}
+export SGA_DATA_DIR=${SGA_DATA_DIR:-/dvs_ro/cfs/cdirs/cosmo/data/sga/2025/data}
+export SGA_HTML_DIR=${SGA_HTML_DIR:-/dvs_ro/cfs/cdirs/cosmo/data/sga/2025/html}
+
 exec ${SGA_PREFIX}/bin/python -m ipykernel -f $connection_file
